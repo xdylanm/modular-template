95a99fcc28cb0d276768006c0b8d9d4c4a88da12
xdylanm <you@example.com>
2025-02-16 11:29:40 -0800

    Migrating template to use RTD schema and .rst (instead of .md) for consistency

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,19 +9,21 @@
 project = 'Template'
 copyright = '2024, Author'
 author = 'Author'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
